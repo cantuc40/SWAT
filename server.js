55e17d98e6003192078406f9b893db1a7639744e
cantuc40 <cantuc40@gator.uhd.edu>
2022-05-26 18:08:20 -0500

    test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const issueRouter = require('./controllers/issue.controller');
 app.use('/api/issues', issueRouter);
 
 
-if (process.env.ENVIRONMENT == 'production'){
+if (process.env.NODE_ENV == 'production'){
     app.use(express.static(path.resolve(__dirname, "../client/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
